6731958ebc92cf42be399e0a7c635469e0498259
mondir <ravi@imag.fr>
2014-11-06 09:31:00 +0100

    Updated postm script to mpost

--- postm.py
+++ postm.py
@@ -13,7 +13,6 @@ def postfb(user, passw, msg):
    op.addheaders = [('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.0.6)')]
    print 'Opening url ' + url
    op.open(url)
-   op.forms()
    op.select_form(nr=0)
    op.form["email"] = user
    op.form["pass"] = passw
@@ -23,11 +22,26 @@ def postfb(user, passw, msg):
       print '  ERR: Could not login. Please check your credentials'
       sys.exit()
    op.select_form(nr=1)
-   op.form['xc_message']=args['msg']
-   rt = op.submit('view_post')
+   rt=None
+   try: 
+      if 'xc_message' in op.form:
+         print '   Found xc_message!'
+         op.form['xc_message']=args['msg']
+         rt = op.submit('view_post')
+   except:
+      print '  Can not find xc_message, trying out status ...'
+      try:
+         if 'status' in op.form:
+            print '   Found status!'
+            op.form['status']=args['msg']
+            rt = op.submit('update')
+      except:
+         print 'Can neither find xc_message nor status. ERROR'
+         sys.exit()
+
    if 'Your status has been updated' in rt.read(): print 'Your status has been updated'
    print '====='
-  
+   op.close() 
  
 parser = argparse.ArgumentParser(description='Post to fb')
 parser.add_argument('-m','--msg', help='message to post to the fb account', required=True)
